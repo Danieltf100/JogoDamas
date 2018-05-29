abf045fa9264c5a322bba9ea9af51bd32d40544a
Danieltf100 <danieltf100@outlook.com>
2018-05-28 22:25:19 -0300

    novas funções em Cenario

--- src/util/Cenario.java
+++ src/util/Cenario.java
@@ -1,24 +1,31 @@
-package model;
+package util;
 
 import java.util.ArrayList;
 
+import model.Jogada;
+import model.TabuleiroFoto;
+
 public class Cenario {
 	
-	private int pontuacao;
+	private long pontuacao;
 	private ArrayList<Cenario> filhos;
 	private TabuleiroFoto tf;
+	private Jogada jogada;
+	private boolean viceKing;
 	
 	public Cenario(TabuleiroFoto tf) {
 		this.setPontuacao(0);
 		this.setFilhos(new ArrayList<>());
 		this.setTf(tf);
+		this.setJogada(new Jogada(0, 0));
+		this.setViceKing(false);
 	}
 
-	public int getPontuacao() {
+	public long getPontuacao() {
 		return pontuacao;
 	}
 
-	public void setPontuacao(int pontuacao) {
+	public void setPontuacao(long pontuacao) {
 		this.pontuacao = pontuacao;
 	}
 
@@ -37,5 +44,30 @@ public class Cenario {
 	public void setTf(TabuleiroFoto tf) {
 		this.tf = tf;
 	}
+
+	public Jogada getJogada() {
+		return jogada;
+	}
+
+	public void setJogada(Jogada jogada) {
+		this.jogada = jogada;
+	}
+
+	public boolean isViceKing() {
+		return viceKing;
+	}
+
+	public void setViceKing(boolean hasViceKing) {
+		this.viceKing = hasViceKing;
+	}
+
+	@Override
+	public String toString() {
+		return "*****\nCenario [pontuacao=" + pontuacao + ", filhos=" + filhos.toString()
+				+ ", \ntf=\n" + tf.toString() + ", jogada=" + jogada.toString() + ", viceKing="
+				+ viceKing + "]\n*****\n";
+	}
+	
+	
 	
 }
