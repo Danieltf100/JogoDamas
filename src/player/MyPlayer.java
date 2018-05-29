96a29523676974d1488e6d75e7aca87870afae26
Danieltf100 <danieltf100@outlook.com>
2018-05-28 22:26:24 -0300

    MyPlayer agora usa a classe Heuristica e joga com ela

--- src/player/MyPlayer.java
+++ src/player/MyPlayer.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import util.Cenario;
+import util.Heuristica;
 import factory.FactoryPlayer;
-import model.Cenario;
+import model.Casa;
 import model.Jogada;
 import model.Jogo;
+import model.Peca;
 import model.Player;
 import model.TabuleiroFoto;
 
@@ -20,66 +23,13 @@ public class MyPlayer extends Player {
 	@Override
 	public Jogada joga(TabuleiroFoto tf) {
 		try {
-			Heuristica h = new Heuristica(5, tf);
-			return h.getMelhorJogada();
+			return Heuristica.getJogada(tf, this.getSide());
 		} catch (Exception e) {
 			Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage());
 			e.printStackTrace();
 		}
-		return null;
-	}
-	
-	private class Heuristica {
-				
-		public Cenario cenarioAtual;
-		public int quantidadeDeCenarios;
-		private Jogada melhorJogada;
-		private TabuleiroFoto tf;
-		
-		/**
-		 * 
-		 * @param Int quantidadeDeCenarios
-		 * @param TabuleiroFoto tf
-		 * @throws QuantidadeDeCenariosMenorQueUmException
-		 */
-		public Heuristica(int quantidadeDeCenarios, TabuleiroFoto tf) throws QuantidadeDeCenariosMenorQueUmException {
-			if (quantidadeDeCenarios<1) throw new QuantidadeDeCenariosMenorQueUmException();
-			cenarioAtual = new Cenario(tf);
-			this.quantidadeDeCenarios = quantidadeDeCenarios;
-			this.tf = tf;
-			this.doSearch();
-		}
-		
-		private void doSearch() {
-			
-		}
-		
-		private ArrayList<Cenario> geraFilhos(TabuleiroFoto tf) {
-			ArrayList<Cenario> filhos = new ArrayList<>();
-						
-			return filhos;
-		}
-		
-		/**
-		 * @return retorna a melhor posiçao para se jogar
-		 */
-		public Jogada getMelhorJogada() {
-			return this.melhorJogada;
-		}
-		/**
-		 * 
-		 * Disparado quando se tenta usar predição menor que 1 grau de profundidade.
-		 *
-		 */
-		public class QuantidadeDeCenariosMenorQueUmException extends Exception {
-			
-			public QuantidadeDeCenariosMenorQueUmException() {
-				super("O valor não pode ser menor do que 1");
-			}
-			
-		}
-		
-	}
+		return new Jogada(0, 0, 0, 0);
+	}	
 	
 	public static void main(String[] args) {
 		TabuleiroFoto tf = new TabuleiroFoto();
@@ -87,9 +37,12 @@ public class MyPlayer extends Player {
 		System.out.println("criando jogador 1");
 		Player p1 = FactoryPlayer.getPlayer('R', tf);
 		System.out.println("criando jogador 2");
-		Player p2 = FactoryPlayer.getPlayer('R', tf);
+		//Player p2 = FactoryPlayer.getPlayer('R', tf);
+		Player p2 = new MyPlayer(tf);
 		System.out.println("criando jogo");
 		Jogo jogo = new Jogo(p1,p2);
+		System.out.println("P1 side is "+p1.getSide());
+		System.out.println("P2 side is "+p2.getSide());
 		System.out.println("comecando jogo");
 		jogo.comeca();
 	}	
